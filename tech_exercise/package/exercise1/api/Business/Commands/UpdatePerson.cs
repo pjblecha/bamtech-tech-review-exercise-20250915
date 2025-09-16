86021b15a3d4317770c9a4590411672234ec2109
pjblecha <44296698+pjblecha@users.noreply.github.com>
2025-09-15 21:47:25 -0500

    first pass

---
+++ tech_exercise/package/exercise1/api/Business/Commands/UpdatePerson.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.EntityFrameworkCore;
+using StargateAPI.Business.Data;
+using StargateAPI.Controllers;
+
+namespace StargateAPI.Business.Commands
+{
+    public class UpdatePerson : IRequest<UpdatePersonResult>
+    {
+        public required string Name { get; set; } = string.Empty;
+    }
+
+    public class UpdatePersonPreProcessor : IRequestPreProcessor<UpdatePerson>
+    {
+        private readonly StargateContext _context;
+        public UpdatePersonPreProcessor(StargateContext context)
+        {
+            _context = context;
+        }
+
+        public Task Process(UpdatePerson request, CancellationToken cancellationToken)
+        {
+            var person = _context.People.AsNoTracking().FirstOrDefault(z => z.Name == request.Name);
+
+            if (person is null) throw new BadHttpRequestException("Bad Request");
+            
+            return Task.CompletedTask;
+        }
+    }
+
+    public class UpdatePersonHandler : IRequestHandler<UpdatePerson, UpdatePersonResult>
+    {
+        private readonly StargateContext _context;
+
+        public UpdatePersonHandler(StargateContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UpdatePersonResult> Handle(UpdatePerson request, CancellationToken cancellationToken)
+        {
+            var person = _context.People.AsNoTracking().First(z => z.Name == request.Name);
+
+            _context.People.Update(person);
+
+            await _context.SaveChangesAsync();
+
+            return new UpdatePersonResult()
+            {
+
+            };
+        }
+    }
+
+    public class UpdatePersonResult : BaseResponse
+    {
+
+    }
+}
+
